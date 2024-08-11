ca7c44c3baeffae71d440ea6bae15c485c9e3fb9
testuserasdfgkw <csgoofficiall2020@gmail.com>
2024-08-11 16:04:37 +0500

    Добавлен тест для проврерки отпарвленных данных пользователя

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -12,13 +12,34 @@ class ItemValidationTest(FunctionalTest):
         '''тест: нельзя добавлять пустые элементы списка'''
         # Эдит открывает домашнюю страницу и случайно пытается отправить
         # пустой элемент списка, Она нажимает enter на пустом поле ввода
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # Домашняя страница обновляется, и появляется сообщение об ошибке,
         # это срабатывает
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You cant have an empty list item"
+        )
+        )
+        # Эдит снова пробует теперь с неким текстом для элемента, и это
+        # теперь срабатывает
+        self.browser.find_element_by_id('id_new_item').send_keys('Купить молоко')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить молоко')
 
         # Как ни странно, Эдит решает отправить второй пустой элемент списка
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # Она получает аналогичное предупреждение на странице списка
-
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You cant have an empty list item"
+        )
+        )
         # И она может его исправить, заполнив поле неким странице списка
-        self.fail('напиши меня!')
+        self.browser.find_element_by_id('id_new_item').send_keys('Сделать чай')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить молоко')
+        self.wait_for_row_in_list_table('2: Сделать чай')
+        self.fail('Закончить тест')
