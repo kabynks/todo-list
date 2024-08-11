7013b26e6f94248b7d1d6021607973f56b5baaa8
testuserasdfgkw <csgoofficiall2020@gmail.com>
2024-08-11 16:18:06 +0500

    Разделены модульные тесты на два файла

---
+++ lists/tests/test_views.py
@@ -0,0 +1,27 @@
+from lists.models import Item, List
+from django.test import TestCase
+
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
+        self.assertTemplateUsed(response, 'lists/list.html')
+    def test_displays_only_items_for_that_list(self):
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
+
+        other_list = List.objects.create()
+        Item.objects.create(text='другой элемент 1 списка', list=other_list)
+        Item.objects.create(text='другой элемент 2 списка', list=other_list)
+        response = self.client.get(f'/lists/{list_.id}/')
+    
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'другой элемент 1 списка')
+        self.assertNotContains(response, 'другой элемент 2 списка')
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
